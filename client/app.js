af0dd52ce50deb781ee60bf7b12b912b22fd421d
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-23 14:35:18 +0200

    Start app, add html, css and js

---
+++ client/app.js
@@ -0,0 +1,41 @@
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection = document.querySelector('#messages-section');
+const messagesList = document.querySelector('#messages-list');
+const addMessageForm = document.querySelector('#add-messages-form');
+const userNameInput = document.querySelector('#username');
+const messageContentInput = document.querySelector('#message-content');
+let userName = '';
+// login form
+loginForm.addEventListener('submit', e => login(e));
+const login = (e) => {
+    e.preventDefault();
+    if (userNameInput.value == '') {
+        alert('What is your name?')
+    } else {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    }
+};
+// send message
+addMessageForm.addEventListener('submit', e => sendMessage(e));
+const sendMessage = (e) => {
+    e.preventDefault();
+    if (messageContentInput.value == '') {
+        alert('What is your message?');
+    } else {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    }
+};
+const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if (author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+        <h3 class="message__author">${author === userName ? 'You' : author}</h3>
+        <div class="message__content">${content}</div>
+    `;
+    messagesList.appendChild(message);
+};
